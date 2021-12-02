37c4d97cceef605850b0ae1aced4c1f8029b11d8
Zendjoker <adoumazzouz.aa@gmail.com>
2021-12-02 14:42:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css"
+import profileImage from './profileImage.png'
+  
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Adam Azouz",
+      bio: "This is my bio",
+      imgSrc: {profileImage},
+      profession: "FullStack Developper"
+    },
+    show: true
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+  
+
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+          <img style={{width:"100px"}}
+            //src={this.state.Person.imgSrc} 
+            src= "https://image.noelshack.com/fichiers/2021/48/4/1638451035-profile-icon-png-916.png"
+            alt="Photo de profil">
+
+            </img>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio} ......</h1>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
-
+  
 export default App;
